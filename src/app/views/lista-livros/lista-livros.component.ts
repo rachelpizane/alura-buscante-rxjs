a74cebc5affffa18abd1aea92d04bdbc2a5ece65
Rachel Pizane <rachel.maia@dbserver.com.br>
2025-04-24 17:46:22 -0300

    feat: incluido mais operadores no observable de busca dos livros

--- src/app/views/lista-livros/lista-livros.component.ts
+++ src/app/views/lista-livros/lista-livros.component.ts
@@ -1,7 +1,7 @@
 import { HttpErrorResponse } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { FormControl } from '@angular/forms';
-import { Observable, Subscription, switchMap, tap } from 'rxjs';
+import { debounce, debounceTime, distinctUntilChanged, filter, Observable, Subscription, switchMap, tap } from 'rxjs';
 import { Livro } from 'src/app/moldes/interfaces/livro';
 import { LivroService } from 'src/app/service/livro/livro.service';
 
@@ -12,11 +12,18 @@ import { LivroService } from 'src/app/service/livro/livro.service';
 })
 export class ListaLivrosComponent {
   campoBusca: FormControl = new FormControl();
+  PAUSE_TIME: number = 500;
 
   livrosEncontrados$: Observable<Livro[]> = this.campoBusca.valueChanges
   .pipe(
     tap((valorDigitado: string) => console.log('Valor digitado:', valorDigitado)),
+    filter((valorDigitado: string) => valorDigitado.length >= 3),
+    tap((valorDigitado: string) => console.log('Valor válido!')),
+    debounceTime(this.PAUSE_TIME),
+    tap((valorDigitado: string) => console.log('Tempo de espera: Finalizado!')),
+    distinctUntilChanged(),
     switchMap((valorDigitado: string) => this.livroService.buscar(valorDigitado)),
+    tap((livros: Livro[]) => console.log(livros)),
   )
 
   constructor(private livroService: LivroService) { }
