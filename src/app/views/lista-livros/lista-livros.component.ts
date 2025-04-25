806f25307fbcb68cb8ed850ab91e03bcc5eecaed
Rachel Pizane <rachel.maia@dbserver.com.br>
2025-04-25 10:55:19 -0300

    feat: incluido captura e tratamento de erro

--- src/app/views/lista-livros/lista-livros.component.ts
+++ src/app/views/lista-livros/lista-livros.component.ts
@@ -1,7 +1,7 @@
 import { HttpErrorResponse } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { FormControl } from '@angular/forms';
-import { debounce, debounceTime, distinctUntilChanged, filter, Observable, Subscription, switchMap, tap } from 'rxjs';
+import { catchError, debounce, debounceTime, distinctUntilChanged, EMPTY, filter, Observable, Subscription, switchMap, tap, throwError } from 'rxjs';
 import { Livro } from 'src/app/moldes/interfaces/livro';
 import { LivroService } from 'src/app/service/livro/livro.service';
 
@@ -12,6 +12,7 @@ import { LivroService } from 'src/app/service/livro/livro.service';
 })
 export class ListaLivrosComponent {
   campoBusca: FormControl = new FormControl();
+  mensagemErro: string = '';
   PAUSE_TIME: number = 500;
 
   livrosEncontrados$: Observable<Livro[]> = this.campoBusca.valueChanges
@@ -24,6 +25,12 @@ export class ListaLivrosComponent {
     distinctUntilChanged(),
     switchMap((valorDigitado: string) => this.livroService.buscar(valorDigitado)),
     tap((livros: Livro[]) => console.log(livros)),
+    catchError((error: HttpErrorResponse) => {
+      console.error(error);
+      this.mensagemErro = 'Erro ao buscar livros! Recarregue a aplicação!';
+      return EMPTY; // opção 1
+      // return throwError(() => new Error(this.mensagemErro = 'Erro ao buscar livros! Recarregue a aplicação!')); // opção 2
+    })
   )
 
   constructor(private livroService: LivroService) { }
